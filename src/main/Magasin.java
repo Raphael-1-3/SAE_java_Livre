5816a6f88933e000c109c6929d3e0d500e8c7ea9
RobinFaucheux <robin.faucheux04@gmail.com>
2025-05-02 11:53:11 +0200

    Ajout du code de base de la classe administrateur

---
+++ src/main/Magasin.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Magasin {
+    private int idMag;
+    private String nomMag;
+    private String villeMag;
+
+    public Magasin(int id, String nom, String ville)
+    {
+        this.idMag = id;
+        this.nomMag = nom;
+        this.villeMag = ville;
+    }
+}
