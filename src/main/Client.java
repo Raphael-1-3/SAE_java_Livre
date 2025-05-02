ebb177bf9e83e2e85b5bca1f2757cdd23d0adc1a
RobinFaucheux <robin.faucheux04@gmail.com>
2025-05-02 11:56:21 +0200

    Ajout de la liste de commandes dans Client

--- src/main/Client.java
+++ src/main/Client.java
@@ -1,10 +1,14 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Client extends User {
     private char modeReception;
     private int codePostal;
     private String villeCli;
     private String adresseCli;
+    private List<Commande> commandes;
 
     public Client(int id, String prenom, String nom, Magasin magasin, int codePostal, String villeCli, String adresseCli)
     {
@@ -13,6 +17,17 @@ public class Client extends User {
         this.codePostal = codePostal;
         this.villeCli = villeCli;
         this.adresseCli = adresseCli;
+        this.commandes = new ArrayList<>();
+    }
+
+    public Client(int id, String prenom, String nom, Magasin magasin, int codePostal, String villeCli, String adresseCli, List<Commande> commandes)
+    {
+        super(id, prenom, nom, magasin);
+        this.modeReception = 'M';
+        this.codePostal = codePostal;
+        this.villeCli = villeCli;
+        this.adresseCli = adresseCli;
+        this.commandes = commandes;
     }
 
     public void faireCommande(){}
