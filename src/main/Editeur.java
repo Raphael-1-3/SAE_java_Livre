32eebc96168aac779eaa7497cf194f9c0df2eb54
CorentinLac <lacoumecorentin@gmail.com>
2025-05-02 11:43:53 +0200

    commit de la classe Editeur

---
+++ src/main/Editeur.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Editeur {
+
+    private int idedit;
+    private String nomedit;
+
+    public Editeur(int idedit,String nomedit){
+
+        this.idedit=idedit;
+        this.nomedit=nomedit;
+    }
+    
+}
