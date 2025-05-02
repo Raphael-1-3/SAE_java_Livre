8c2139c648946200563b42846437e02720777b66
RobinFaucheux <robin.faucheux04@gmail.com>
2025-05-02 11:48:05 +0200

    Ajout du code de base de Vendeur

---
+++ src/main/Vendeur.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Vendeur extends User{
+    public Vendeur(int id , String prenom, String nom, Magasin mag)
+    {
+        super(id, prenom, nom, mag);
+    }
+
+    public void ajouterLivre(){}
+
+    public void updateStock() {}
+
+    public void disponibilites() {}
+
+    public void passerCommande() {}
+
+    public void Transfer() {}
+
+}
