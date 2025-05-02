591d0005876cd5e472aac177231606701f885cf9
Lacoume Corentin <o22407142@or-iut-i004-f27.iut45.univ-orleans.fr>
2025-05-02 11:34:10 +0200

    commit de la classe Auteur

---
+++ src/main/Auteur.java
@@ -0,0 +1,16 @@
+public class Auteur{
+
+    private int idAut;
+    private String nomAuteur;
+    private String anneeNaissance;
+    private String anneeMort;
+    private boolean estmort;
+
+
+    public Auteur(int idAut,String nom,String anneeN,String anneeM){
+        this.idAut=idAut;
+        this.nomAuteur=nom;
+        this.anneeMort=anneeM;
+        this.anneeNaissance=anneeN;
+    }
+}
