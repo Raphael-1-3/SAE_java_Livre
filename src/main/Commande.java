84682cacff228941c38323700b65255937c883cf
CorentinLac <lacoumecorentin@gmail.com>
2025-05-16 12:12:42 +0200

    Implémentation terminale coco

--- src/main/Commande.java
+++ src/main/Commande.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Commande{
 
@@ -48,5 +49,55 @@ public class Commande{
         return res;
     }
     
-    public void changerModeReception(){}
+    public static void changerModeReception(){
+        List<String> maListe = new ArrayList<>();
+        maListe.add("Magasin");
+        maListe.add("Livraison");
+        maListe.add("Retour");
+        boolean commande_faite = false;
+        while(!commande_faite){
+            System.out.println(AfficherMenu.Menu("Choix de Livraison",maListe)); 
+            System.out.println("Ou souhaite tu récuperer ta commande ? : ");
+            String commande_brute=System.console().readLine();
+            String commande = commande_brute.strip().toLowerCase();
+            if (commande.equals("1")){
+                choisirMagasin();
+
+            }
+
+
+
+            else if(commande.equals("3")){
+                commande_faite=true;
+            }
+
+
+        }
+        
+        
+    }
+
+    public static void choisirMagasin(){
+        List<String> maListe = new ArrayList<>();
+        maListe.add("Magasin Robin");
+        maListe.add("Magasin Coco");
+        maListe.add("Magasin Raphael");
+        maListe.add("Magasin Jorris");
+        maListe.add("Retour");
+        boolean commande_faite = false;
+        while(!commande_faite){
+            System.out.println(AfficherMenu.Menu("Choix Magasins",maListe));
+            System.out.println("Dans quel magasin ? : ");
+            String commande_brute=System.console().readLine();
+            String commande = commande_brute.strip().toLowerCase();
+            if (commande.equals("5")){
+                commande_faite=true;
+
+            }
+
+        
+
+        }
+    }
+
 }
