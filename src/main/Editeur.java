0189d33b3d6a07aef7fec666d97bc38e7acd746b
RobinFaucheux <robin.faucheux04@gmail.com>
2025-05-16 11:28:41 +0200

    ajout des getteurs pour Editeur

--- src/main/Editeur.java
+++ src/main/Editeur.java
@@ -5,10 +5,18 @@ public class Editeur {
     private int idedit;
     private String nomedit;
 
-    public Editeur(int idedit,String nomedit){
-
+    public Editeur(int idedit, String nomedit){
         this.idedit=idedit;
         this.nomedit=nomedit;
     }
-    
+
+    public int getIdEditeur()
+    {
+        return this.idedit;
+    }
+
+    public String getNomEdit()
+    {
+        return this.nomedit;
+    }
 }
