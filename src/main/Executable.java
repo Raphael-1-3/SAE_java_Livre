67967693a637bb5bd8a775c6fac374ed9dff7a86
CorentinLac <lacoumecorentin@gmail.com>
2025-05-16 16:55:54 +0200

    ajout inerface non fonctionnel coco

--- src/main/Executable.java
+++ src/main/Executable.java
@@ -3,6 +3,7 @@ package main;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Executable{
     public static void main (String [] args)
