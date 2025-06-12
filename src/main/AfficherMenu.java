9987bac7b06b51d1d27dd24830e327f45ccc41c1
Raphael-1-3 <raphael.brousseau@etu.univ-orleans.fr>
2025-06-12 19:29:49 +0200

    ajout des menu client pour la recherche de livre par nom et ajout au panier du client

--- src/main/AfficherMenu.java
+++ src/main/AfficherMenu.java
@@ -1,25 +1,48 @@
 package main;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.math.RoundingMode;
 public class AfficherMenu 
 {
-    private AfficherMenu(){}
-    public static String Menu(String titre, List<String> options)
-    {
-        String str = "╭────────────────────────────╮ \n"
-        + "│" + " ".repeat(14 - ((int) (titre.length()/2))) + titre + " ".repeat(14 - ((int) Math.round(titre.length()/2.0))) + "│\n"
-        + "├────────────────────────────┤\n";
-        int i = 1;
-        for (String opt : options)
-        {
-            String istr = ""  +i;
-            str = str + "│ " + i + ". " + opt + " ".repeat(25 - (istr.length() + opt.length())) + "│\n";
-            i++;
-        }
-        str  = str + "╰────────────────────────────╯\n";
-        return str;
-    }
+   public static String Menu(String titre, List<String> options) {
+        // Trouver la longueur maximale parmi le titre et les options
+        String motLePlusLong = Collections.max(options, Comparator.comparingInt(String::length));
+        int maxOptionLength = motLePlusLong.length();
+        int maxNumLength = String.valueOf(options.size()).length(); // taille du plus grand numéro (1, 10, 100...)
+        int optionLineLength = maxNumLength + 2 + 1 + maxOptionLength; // ex: "10. OptionTexte"
+        
+        int titreLength = titre.length();
+        int contentWidth = Math.max(optionLineLength, titreLength);
+        int boxWidth = contentWidth + 4; // Ajout des marges et bords : "│ " + content + " │"
 
-    
+        StringBuilder str = new StringBuilder();
+
+        // Ligne du haut
+        str.append("╭").append("─".repeat(boxWidth - 2)).append("╮\n");
+
+        // Ligne du titre, centré
+        int paddingLeft = (boxWidth - 2 - titreLength) / 2;
+        int paddingRight = boxWidth - 2 - titreLength - paddingLeft;
+        str.append("│").append(" ".repeat(paddingLeft)).append(titre)
+           .append(" ".repeat(paddingRight)).append("│\n");
+
+        // Séparateur
+        str.append("├").append("─".repeat(boxWidth - 2)).append("┤\n");
+
+        // Lignes des options
+        for (int i = 0; i < options.size(); i++) {
+            String numero = (i + 1) + ".";
+            String opt = options.get(i);
+            String ligne = numero + " " + opt;
+            int espaceFin = contentWidth - ligne.length();
+            str.append("│ ").append(ligne).append(" ".repeat(espaceFin)).append(" │\n");
+        }
+
+        // Ligne du bas
+        str.append("╰").append("─".repeat(boxWidth - 2)).append("╯\n");
+
+        return str.toString();
+    }
 }
--- src/main/Client.java
+++ src/main/Client.java
@@ -12,6 +12,7 @@ public class Client extends User {
     private String adresseCli;
     private String prenom;
     private List<Commande> commandes;
+    private List<Livre> panier;
 
     // Ajout des paramètres manquants pour User : email, motDePasse, role
     public Client(int id, String email, String nom, String prenom, String motDePasse, String role, int codePostal, String villeCli, String adresseCli) {
@@ -21,6 +22,7 @@ public class Client extends User {
         this.villeCli = villeCli;
         this.adresseCli = adresseCli;
         this.commandes = new ArrayList<>();
+        this.panier = new ArrayList<>();
     }
 
     public Client(int id, String email, String nom, String prenom, String motDePasse, String role, int codePostal, String villeCli, String adresseCli, List<Commande> commandes) {
@@ -30,6 +32,7 @@ public class Client extends User {
         this.villeCli = villeCli;
         this.adresseCli = adresseCli;
         this.commandes = commandes;
+        this.panier = new ArrayList<>();
     }
 
     public int getCodePostal() {
@@ -48,6 +51,17 @@ public class Client extends User {
     public String getPrenom() {
         return this.prenom;
     }
+
+    public void ajoutePanier(Livre livre)
+    {
+        this.panier.add(livre);
+    }
+
+    public List<Livre> getPanier()
+    {
+        return this.panier;
+    }
+
 
     @Override
     public boolean equals(Object o) {
@@ -124,46 +138,184 @@ public class Client extends User {
         System.out.println("╰────────────────────────────╯");
     }
 
-    public static void application(ActionBD bd, User client) {
-        List<String> maListe = new ArrayList<>();
-        maListe.add("Commander");
-        maListe.add("Catalogue");
-        maListe.add("Choisir un magasin");
-        maListe.add("Paramètres");
-        maListe.add("Quitter");
+    public static void application(ActionBD bd, User client) throws SQLException
+    {
+        Client clientC = (Client) client;
+        List<String> menuListe = new ArrayList<>();
+        menuListe.add("Commander un panier");
+        menuListe.add("rechercher un Livre");
+        menuListe.add("consulter Panier");
+        menuListe.add("Paramètres");
+        menuListe.add("Quitter");
 
         boolean commande_faite = false;
         Scanner scanner_test = new Scanner(System.in);
         while (!commande_faite) {
-            System.out.println(AfficherMenu.Menu("Application", maListe));
+            System.out.println(AfficherMenu.Menu("Application", menuListe));
             System.out.println("Que veut tu faire ? : ");
             String commande_brute = scanner_test.nextLine();
             String commande = commande_brute.strip().toLowerCase();
 
-            if (commande.equals("4")) {
+            // On suppose que l'utilisateur entre un numéro de menu (1, 2, 3, ...)
+            switch (commande) {
+            case "1":
+                System.out.println("emmene vers le menu des commade");
+                // TODO: appeler la méthode pour commander un panier
+                break;
+            case "2":
+                System.out.println("emmene vers le catalogue");
+                rechercheLivre(bd, clientC);
+                break;
+            case "3":
+                System.out.println("emmene vers consulter Panier");
+                // TODO: afficher le panier du client
+                menuPanier(bd, clientC);
+                break;
+            case "4":
+                System.out.println("emmene vers parametre pour changer mot de passe");
+                // TODO: afficher les paramètres
+                break;
+            case "5":
+            case "q":
+            case "quitter":
+                System.out.println("la tu quitte lappli client");
                 commande_faite = true;
-            }
-            if (commande.equals("1")) {
-                commande_faite = true;
-            }
-            if (commande.equals("2")) {
-                rechercheLivre(bd);
-            }
-            if (commande.equals("3")) {
-                Commande.menu_rechercher();
+                break;
+            default:
+                System.out.println("Commande non reconnue, veuillez entrer un numéro valide.");
             }
         }
     }
 
+    public static void rechercheLivre(ActionBD bd, Client client) throws SQLException {
+        List<String> menuListe = new ArrayList<>();
+        menuListe.add("Auteur");
+        menuListe.add("Nom de livre");
+        menuListe.add("Quitter");
+
+        boolean commande_faite = false;
+        Scanner scanner = new Scanner(System.in);
+        while (!commande_faite) {
+            System.out.println(AfficherMenu.Menu("Recherche de livre", menuListe));
+            System.out.print("Que veux-tu faire ? : ");
+            String commande_brute = scanner.nextLine();
+            String commande = commande_brute.strip().toLowerCase();
+
+            switch (commande) {
+                case "1":
+                    System.out.println("Recherche par auteur non implémentée.");
+                    break;
+                case "2":
+                    System.out.print("Entrez le titre du livre : ");
+                    String titreRecherche = scanner.nextLine().strip();
+                    List<Livre> tabLiv = bd.cherhcherLivreApproximative(titreRecherche);
+                    if (tabLiv == null || tabLiv.isEmpty()) {
+                        System.out.println("Aucun livre trouvé.");
+                        break;
+                    }
+                    List<String> tabLivreNom = new ArrayList<>();
+                    for (Livre livre : tabLiv) {
+                        tabLivreNom.add(livre.getTitre());
+                    }
+                    boolean choixFait = false;
+                    while (!choixFait) {
+                        System.out.println(AfficherMenu.Menu("Livres trouvés", tabLivreNom));
+                        System.out.print("Entrez le numéro du livre choisi : ");
+                        String choix = scanner.nextLine().strip();
+                        try {
+                            int indice = Integer.parseInt(choix) - 1 ;
+                            if (indice < 0 || indice >= tabLiv.size()) {
+                                System.out.println("Numéro invalide.");
+                                continue;
+                            }
+                            Livre livreChoisi = tabLiv.get(indice);
+                            System.out.print("Voulez-vous ajouter ce livre au panier ? (oui/non) : ");
+                            String reponse = scanner.nextLine().strip().toLowerCase();
+                            if (reponse.equals("oui") || reponse.equals("o")) {
+                                System.out.println("Le livre a été ajouté au panier.");
+                                client.ajoutePanier(livreChoisi);
+                                choixFait = true;
+                            } else if (reponse.equals("non") || reponse.equals("n")) {
+                                choixFait = true;
+                            } else {
+                                System.out.println("Réponse non reconnue, veuillez répondre par oui ou non.");
+                            }
+                        } catch (NumberFormatException e) {
+                            System.out.println("Votre entrée est incorrecte.");
+                        }
+                    }
+                    break;
+                case "3":
+                case "q":
+                case "quitter":
+                    System.out.println("Vous quittez la recherche.");
+                    commande_faite = true;
+                    break;
+                default:
+                    System.out.println("Commande non reconnue, veuillez entrer un numéro valide.");
+            }
+        }
+    }
+
+    public static void menuPanier(ActionBD bd, Client client) 
+    { 
+        List<String> menuListe = new ArrayList<>();
+        menuListe.add("Afficher le panier");
+        menuListe.add("Commander le panier");
+        menuListe.add("Retour");
+
+        Scanner scanner = new Scanner(System.in);
+        boolean quitter = false;
+        while (!quitter) {
+            System.out.println(AfficherMenu.Menu("Menu Panier", menuListe));
+            System.out.print("Que veux-tu faire ? : ");
+            String choix = scanner.nextLine().strip().toLowerCase();
+
+            switch (choix) {
+                case "1":
+                    List<Livre> panier = client.getPanier();
+                    if (panier.isEmpty()) {
+                        System.out.println("Votre panier est vide.");
+                    } else {
+                        System.out.println("Contenu du panier :");
+                        int i = 1;
+                        for (Livre livre : panier) {
+                            System.out.println(i + ". " + livre);
+                            i++;
+                        }
+                    }
+                    break;
+                case "2":
+                    if (client.getPanier().isEmpty()) {
+                        System.out.println("Votre panier est vide, impossible de commander.");
+                    } else {
+                        // Ici, il faudrait appeler la méthode de commande réelle
+                        System.out.println("Commande du panier en cours...");
+                        // Exemple : bd.commanderPanier(client);
+                        System.out.println("Votre commande a été prise en compte !");
+                        client.getPanier().clear();
+                    }
+                    break;
+                case "3":
+                case "q":
+                case "retour":
+                    quitter = true;
+                    break;
+                default:
+                    System.out.println("Commande non reconnue, veuillez entrer un numéro valide.");
+            }
+        }
+    }
+    
     public static void choisirMagasin(ActionBD bd){
-        List<String> maListe = new ArrayList<>();
+        List<String> menuListe = new ArrayList<>();
         List<Magasin> tabMag = null; //bd.listeMagasin()
         //parcour + ajout menu
-        maListe.add("");
-        maListe.add("Retour");
+        menuListe.add("");
+        menuListe.add("Retour");
         boolean commande_faite = false;
         while(!commande_faite){
-            System.out.println(AfficherMenu.Menu("Choix Magasins",maListe));
+            System.out.println(AfficherMenu.Menu("Choix Magasins",menuListe));
             System.out.println("Dans quel magasin ? : ");
             String commande_brute=System.console().readLine();
             String commande = commande_brute.strip().toLowerCase();
@@ -171,15 +323,7 @@ public class Client extends User {
                 commande_faite=true;
 
             }
-
-        
-
         }
-    }
-
-    public static  void rechercheLivre(ActionBD bd) 
-    {
-        //TODO   fair en sorte que cette fonction appel cherhcherLivreApproximative(String nomApproximativeLivre)
     }
 
     public void avoirRecommandations() {}
