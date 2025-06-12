5de395b2f3b486b10861c0e47bea3048c10994c8
RobinFaucheux <robin.faucheux@etu.univ-orleans.fr>
2025-06-12 18:25:48 +0200

    Clean de Magasin

--- src/main/Magasin.java
+++ src/main/Magasin.java
@@ -8,41 +8,19 @@ public class Magasin {
     private int idMag;
     private String nomMag;
     private String villeMag;
-    private HashMap<Livre, Integer> catalogue;
     
 
-    public Magasin(int id, String nom, String ville, HashMap<Livre, Integer> catalogue)
+    public Magasin(int id, String nom, String ville)
     {
         this.idMag = id;
         this.nomMag = nom;
         this.villeMag = ville;
-        this.catalogue= catalogue;
     }
 
     public int getIdmag(){return this.idMag;}
     public String getNomMag(){return this.nomMag;}
     public String getVilleMag(){return this.villeMag;}
     
-
-    public void addLivre(Livre l) 
-    {
-        if (!this.catalogue.containsKey(l))
-        {
-            this.catalogue.put(l, 1);
-        }
-    }
-
-    public void addLivre(Livre l, Integer qte)
-    {
-        if (!this.catalogue.containsKey(l))
-        {
-            this.catalogue.put(l, qte);
-        }
-        else
-        {
-            this.catalogue.put(l, this.catalogue.get(l) + qte);
-        }
-    }
 
 }
 
