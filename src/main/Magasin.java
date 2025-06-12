a9fcddb2f363873f0143dfffb72d1a93089cd70e
RobinFaucheux <robin.faucheux@etu.univ-orleans.fr>
2025-06-12 18:23:16 +0200

    Mise a jour de Magasin

--- src/main/Magasin.java
+++ src/main/Magasin.java
@@ -2,21 +2,21 @@ package main;
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Magasin {
     private int idMag;
     private String nomMag;
     private String villeMag;
-    private List<Livre> listeLivre;
+    private HashMap<Livre, Integer> catalogue;
     
 
-    public Magasin(int id, String nom, String ville)
+    public Magasin(int id, String nom, String ville, HashMap<Livre, Integer> catalogue)
     {
         this.idMag = id;
         this.nomMag = nom;
         this.villeMag = ville;
-        this.listeLivre= new ArrayList<>();
-
+        this.catalogue= catalogue;
     }
 
     public int getIdmag(){return this.idMag;}
@@ -24,38 +24,24 @@ public class Magasin {
     public String getVilleMag(){return this.villeMag;}
     
 
-    public static void rechercheLivre(Magasin mag){
-        List<String> maListe = new ArrayList<>();
-        maListe.add("Magasin1");
-        maListe.add("Magasin2");
-        maListe.add("Retour");
-        boolean commande_faite = false;
-        while(!commande_faite){
-            System.out.println(AfficherMenu.Menu("Recherche", maListe));
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Quel est le titre du livre que tu recherche ? :" );
-            String nomLivre = scanner.nextLine();
-            estPresent(mag,nomLivre);
-
-        }
-    }
-
-    public static void estPresent(Magasin mag,String l){
-        for(Livre livre:mag.listeLivre){
-            if(livre.getTitre().equals(l)){
-            System.out.println("Le livre "+livre.getTitre()+" est présent dans le magasin "+mag.getNomMag());
-        }
-        
-        else{
-            System.out.println("Le livre "+livre.getTitre()+" n'est pas présent dans le magasin "+mag.getNomMag());
-        }
-
-        }
-    }
-
-    public static void ajouterLivre(Magasin mag)
+    public void addLivre(Livre l) 
     {
-        
+        if (!this.catalogue.containsKey(l))
+        {
+            this.catalogue.put(l, 1);
+        }
+    }
+
+    public void addLivre(Livre l, Integer qte)
+    {
+        if (!this.catalogue.containsKey(l))
+        {
+            this.catalogue.put(l, qte);
+        }
+        else
+        {
+            this.catalogue.put(l, this.catalogue.get(l) + qte);
+        }
     }
 
 }
